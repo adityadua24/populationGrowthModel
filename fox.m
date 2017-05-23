aabe205e9e91bc613adc5d5a45ff3739ca397917
Aditya Dua <adityadua24@gmail.com>
2017-05-24 00:01:48 +1000

    work in progress

--- fox.m
+++ fox.m
@@ -4,14 +4,38 @@ classdef fox < handle
     
     properties
         location
+        step_size
     end
     
     methods
-         function [x, y] = spawn(obj)
-             x = randi([1 500]);
-             y = randi([1 500]);
-             
+         function spawn(obj)
+             obj.location = [randi([1 500]) randi([1 500])];      
          end
+         function step(obj)
+             r = rand;
+             if (r > 0) && (r <= 0.125)
+                 obj.location(2) = obj.location(2) + obj.step_size; % y++
+             elseif (r > 0.125) && (r <= 0.250)
+                 obj.location(2) = obj.location(2) + obj.step_size; % y++
+                 obj.location(1) = obj.location(1) + obj.step_size; % x++
+             elseif (r > 0.250) && (r <= 0.375)
+                 obj.location(1) = obj.location(1) + obj.step_size; % x++
+             elseif (r > 0.375) && (r <= 0.500)
+                 obj.location(1) = obj.location(1) + obj.step_size; % x++
+                 obj.location(2) = obj.location(2) - obj.step_size; % y--
+             elseif (r > 0.500) && (r <= 0.625)
+                 obj.location(2) = obj.location(2) - obj.step_size; % y--
+             elseif (r > 0.625) && (r <= 0.750)
+                 obj.location(2) = obj.location(2) - obj.step_size; % y--
+                 obj.location(1) = obj.location(1) - obj.step_size; % x--
+             elseif (r > 0.750) && (r <= 0.875)
+                 obj.location(1) = obj.location(1) - obj.step_size; % x--
+             else
+                 obj.location(2) = obj.location(2) + obj.step_size; % y++
+                 obj.location(1) = obj.location(1) - obj.step_size; % x--
+             end
+         end
+         
     end
     
 end
--- popluationGrowthModel.m
+++ popluationGrowthModel.m
@@ -4,15 +4,28 @@ c = 500;
 cellGrid = cell(500, 500);
 
 numFoxes = 10;
-numRabits = 20;
+numRabbits = 20;
 
-foxes = repmat(fox, 1, numFoxes);
-%rabbits = rabbit(rabbit, 1, numRabits);
+rabbit_step_size = 2;
+fox_step_size = 1;
 
-% Spawn Foxes
+foxes_array = cell(1, numFoxes);
+rabbits_array = cell(1, numRabbits);
+
+% initialise and Spawn Foxes
 for i=1:numFoxes
-    selectFox = foxes(i);
-    fox.spawn(selectFox);
+    foxes_array{i} = fox;
+    foxes_array{i}.step_size = fox_step_size; % Add class constructor this is ugly as fk
+    spawn(foxes_array{i});
 end
+
+% initialise and spawn Rabbits
+for i=1:numRabbits
+    rabbits_array{i} = rabbit;
+    rabbits_array{i}.step_size = rabbit_step_size; % Add class constructor this is ugly as fk
+    spawn(rabbits_array{i});
+end
+
+
 
 
